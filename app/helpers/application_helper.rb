710628925f05a72d10525da18cd79fd486232748
Samuel Tonini <tonini.samuel@gmail.com>
2011-01-03 15:37:22 +0100

    change avatar image default size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   include ActsAsTaggableOn::TagsHelper
 
   def avatar_url(user, options = {})
-    opts = {:size => 100}.merge(options)
+    opts = {:size => 48}.merge(options)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{opts[:size]}"
   end
