286dfb00a152cf33a54a21fba470f3e7ee2ced4a
Yves Senn <yves.senn@gmail.com>
2011-01-06 20:59:06 +0100

    add email notification flag to users

---
+++ db/migrate/20110106195603_add_email_notification_flag_to_users.rb
@@ -0,0 +1,9 @@
+class AddEmailNotificationFlagToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :receive_email_notifications, :boolean
+  end
+
+  def self.down
+    remove_column :users, :receive_email_notifications
+  end
+end
