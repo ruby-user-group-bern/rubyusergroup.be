2e68d9d5b6edd879e95d79f1746912aebc20059e
Samuel Tonini <tonini.samuel@gmail.com>
2011-01-27 07:54:44 +0100

    create newsletter model

---
+++ app/models/newsletter.rb
@@ -0,0 +1,6 @@
+class Newsletter < ActiveRecord::Base
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110125142710) do
+ActiveRecord::Schema.define(:version => 20110127063449) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(:version => 20110125142710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "author_id"
+  end
+
+  create_table "newsletters", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", :force => true do |t|
