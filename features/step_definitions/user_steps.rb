182d27e5c27c083fbca9604798964d2f63582a6b
Yves Senn <yves.senn@gmail.com>
2011-02-12 14:57:41 +0100

    Cucumber specification for new email sending mechanism

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,6 +2,7 @@ Given /^the following users:$/ do |user_table|
   user_table.hashes.each do |row|
     attributes = {}
     attributes[:id] = row['ID'].to_i if row['ID'].present?
+    attributes[:email] = row['E-Mail'] if row['E-Mail'].present?
     attributes[:email] = "#{row['Username']}@gmail.com" if row['Username'].present?
     put_model(:user, row['Username'], attributes)
   end
